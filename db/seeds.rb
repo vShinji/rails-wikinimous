d579514bd904702740d7bebbefbd1140e55ae566
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2023-11-07 21:15:31 +0900

    css done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  Article.create(title: Faker::BossaNova.song, content: Faker::Lorem.sentence(word_count: 10))
+end
